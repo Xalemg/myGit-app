6fdcdad0722b79a6b606e7311345b3e4e55da495
xalemg <xalemg@gmail.com>
2019-04-16 22:03:23 +0200

    started working on the menuApp

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MenuAppBar from "../MenuAppBar/MenuAppBar";
+
+class Home extends Component<any, any> {
+    render()  {
+        return(
+        <div>
+        <MenuAppBar></MenuAppBar>
+        </div>
+        )
+    }
+}
+export default Home;
---
+++ src/containers/landingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component<any, any> {
+    render()  {
+        return(
+        <div>
+            Landing Page
+        </div>
+        )
+    }
+}
+export default LandingPage;
